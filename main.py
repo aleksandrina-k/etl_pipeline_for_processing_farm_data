122c2a0137ee08fc3b748a922fd30c88787679ba
akaradalieva <akaradalieva@lelypartners.com>
2024-05-27 19:59:13 +0300

    combine all visualization jobs into one

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# from jobs.create_dashboards import CreateDashboards
 from jobs.load_data_from_warehouse import LoadDataFromWarehouse
 from jobs.process_bronze_to_silver0 import ProcessBronzeToSilver0
 from jobs.process_silver0_to_silver1 import ProcessSilver0ToSilver1
@@ -7,13 +8,14 @@ if __name__ == "__main__":
     # TODO: fix wrongly encoded strings in the ration and feed names
     # TODO: convert all columns to snake_case
     # TODO: fix the KPIs after grouping. Need to be calculated correctly.
+    # TODO: figure out why there are duplicates in the gold layer tables
     config_file_path = r"conf/load_data_from_warehouse.json"
 
     task1 = LoadDataFromWarehouse(config_file_path)
     task2 = ProcessBronzeToSilver0(config_file_path)
     task3 = ProcessSilver0ToSilver1(config_file_path)
     task4 = ProcessSilver1ToGold(config_file_path)
-    # vis = Visualization(config_file_path)
+    # vis = CreateDashboards(config_file_path)
 
     task1.launch()
     task2.launch()
